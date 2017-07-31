e7103b252c4525f4d5036b0989b2ab622d21e0f9
Ivan E. Rodriguez <iv.rodriguez@ivrodriguez.com>
2017-07-31 07:20:22 -0400

    Initial Commit

---
+++ nuntius/nuntius.h
@@ -0,0 +1,19 @@
+//
+//  nuntius.h
+//  nuntius
+//
+//  Created by Ivan E. Rodriguez on 2017-07-31.
+//  Copyright © 2017 Ivan E. Rodriguez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for nuntius.
+FOUNDATION_EXPORT double nuntiusVersionNumber;
+
+//! Project version string for nuntius.
+FOUNDATION_EXPORT const unsigned char nuntiusVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <nuntius/PublicHeader.h>
+
+
